b62e8b8976e23d8a072f7944fba6cddffa338c6d
Marshall Ward <git@marshallward.org>
2016-04-26 19:23:41 +1000

    Method lookup table header
    Lookup tables for uvec method functions moved to an external header,
    methods.h.

--- inc/types.h
+++ inc/types.h
@@ -25,28 +25,4 @@ typedef enum _uv_type {
 
 #define UV_NTYPES (UV_LONGDOUBLE + 1)
 
-/* TODO: I don't know how to set the values of uv_tsize */
-extern size_t uv_tsize;
-
-/*
-uv_tsize[UV_CHAR] = sizeof(char);
-uv_tsize[UV_SCHAR] = sizeof(signed char);
-uv_tsize[UV_UCHAR] = sizeof(unsigned char);
-
-uv_tsize[UV_SHORT] = sizeof(short);
-uv_tsize[UV_USHORT] = sizeof(unsigned short);
-
-uv_tsize[UV_INT] = sizeof(int);
-uv_tsize[UV_UINT] = sizeof(int);
-
-uv_tsize[UV_LONG] = sizeof(long);
-uv_tsize[UV_ULONG] = sizeof(unsigned long);
-
-uv_tsize[UV_LONGLONG] = sizeof(long long);
-uv_tsize[UV_ULONGLONG] = sizeof(unsigned long long);
-
-uv_tsize[UV_FLOAT] = sizeof(float);
-uv_tsize[UV_DOUBLE] = sizeof(double);
-uv_tsize[UV_LONGDOUBLE] = sizeof(long double);
-*/
 #endif /* _UVTYPES_H_ */
--- src/uvec.c
+++ src/uvec.c
@@ -3,6 +3,7 @@
 #include <string.h> /* memcpy */
 
 #include "uvec.h"
+#include "methods.h"
 
 uvec * uv_create(uv_type dtype, int ndims, int *dims)
 {
@@ -11,62 +12,6 @@ uvec * uv_create(uv_type dtype, int ndims, int *dims)
     size_t itemsize;
 
     int i;
-
-    /* Datatype lookup tables */
-    /* TODO: Define externally? */
-
-    static size_t dtypesize[UV_NTYPES] = {
-        sizeof(char),               /* UV_CHAR */
-        sizeof(signed char),        /* UV_SCHAR */
-        sizeof(unsigned char),      /* UV_UCHAR */
-        sizeof(short),              /* UV_SHORT */
-        sizeof(unsigned short),     /* UV_USHORT */
-        sizeof(int),                /* UV_INT */
-        sizeof(unsigned int),       /* UV_UINT */
-        sizeof(long),               /* UV_LONG */
-        sizeof(unsigned long),      /* UV_ULONG */
-        sizeof(long long),          /* UV_LONGLONG */
-        sizeof(unsigned long long), /* UV_ULONGLONG */
-        sizeof(float),              /* UV_FLOAT */
-        sizeof(double),             /* UV_DOUBLE */
-        sizeof(long double)         /* UV_LONGDOUBLE */
-    };
-
-    static int (*uv_add_funcs[UV_NTYPES])(uvec *t, uvec *u, uvec *v) = {
-        NULL, /* UV_CHAR */
-        NULL, /* UV_SCHAR */
-        NULL, /* UV_UCHAR */
-        NULL, /* UV_SHORT */
-        NULL, /* UV_USHORT */
-        uv_add_int, /* UV_INT */
-        NULL, /* UV_UINT */
-        NULL, /* UV_LONG */
-        NULL, /* UV_ULONG */
-        NULL, /* UV_LONGLONG */
-        NULL, /* UV_ULONGLONG */
-        NULL, /* UV_FLOAT */
-        NULL, /* UV_DOUBLE */
-        NULL, /* UV_LONGDOUBLE */
-    };
-
-    /* TODO: They can't all return `int`... */
-    /*       But sum needs to support vector output anyway */
-    static int (*uv_sum_funcs[UV_NTYPES])(uvec *u) = {
-        NULL, /* UV_CHAR */
-        NULL, /* UV_SCHAR */
-        NULL, /* UV_UCHAR */
-        NULL, /* UV_SHORT */
-        NULL, /* UV_USHORT */
-        uv_sum_int, /* UV_INT */
-        NULL, /* UV_UINT */
-        NULL, /* UV_LONG */
-        NULL, /* UV_ULONG */
-        NULL, /* UV_LONGLONG */
-        NULL, /* UV_ULONGLONG */
-        NULL, /* UV_FLOAT */
-        NULL, /* UV_DOUBLE */
-        NULL, /* UV_LONGDOUBLE */
-    };
 
     /* Object fields */
 
@@ -90,7 +35,7 @@ uvec * uv_create(uv_type dtype, int ndims, int *dims)
         v->dims[i] = dims[i];
 
     /* Method assignment */
-    /* TODO: Pre-calculated hash tables */
+
     v->add = uv_add_funcs[dtype];
     v->sum = uv_sum_funcs[dtype];
 
